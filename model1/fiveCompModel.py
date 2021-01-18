8d44485ddfa1bd4bc23413c3cb1c7d5b9681e484
Adel elmala <adel.elmala2025@gmail.com>
2021-01-19 01:50:46 +0200

    avg input resistance mesurement

--- model1/fiveCompModel.py
+++ model1/fiveCompModel.py
@@ -5,8 +5,9 @@ class FiveCompModel():
         
         self.model = NrnModel("5CompMy_temp.hoc")
     
-    def inputResistance(self):
-        amp = -0.5
+    def inputResistance(self,amp):
+        # amp = -0.5
+        amp = amp
         stim = self.model.setIClamp(delay=150,duration=100,amp=amp,segment=self.model.soma,position=0.5)
         volt,t = self.model.recordVolt(self.model.soma,0.5)
         self.model.runControler(TStop= 500,init = -65 )
@@ -17,10 +18,12 @@ class FiveCompModel():
         # print(self.soma.psection())
 
         print("----- Input Resistance Measurement -----")
+        print(f'clamp Current: {amp} nA')
         print(f'restMembPot: {restMembPot} mV')
         print(f'minDepolarPot: {minDepolarPot} mV')
         print(f'inputResistance: {inputResistance} (mV/nA)')
-        self.model.graphVolt(volt,t,label='soma(0.5)')
+        # self.model.graphVolt(volt,t,label='soma(0.5)')
+        print("----- ---------------------------- -----")
         
         return inputResistance
 
@@ -29,14 +32,21 @@ class FiveCompModel():
 
         capacitance = self.model.soma.psection()['cm'][0]
         # print(capacitance)
-        tau = capacitance * inputResistance;
+        tau = capacitance * inputResistance
+        print("----- ---------------------------- -----")
+
         return tau
 
 
 if __name__=='__main__':
 
     modelRun = FiveCompModel()
-    inputRes = modelRun.inputResistance()
-    tau = modelRun.timeConstant(inputRes)
+    testAmps = [-0.5,-0.6,-0.7,-0.8,-0.9,-1.0]
+    
+    inputRes = [modelRun.inputResistance(amp) for amp in testAmps]
+    avgInRes = sum(inputRes)/len(inputRes)
+    print(f'inputRes List: {inputRes}')
+    print(f'avgInRes: {avgInRes} (mV/nA)' )
+    # tau = modelRun.timeConstant(inputRes)
 
-    print(f'Tau: {tau}')
+    # print(f'Tau: {tau}')
